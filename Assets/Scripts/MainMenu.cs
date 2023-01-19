847058f2d7659ce873bbff705e3f8dbfca2229b5
Radwa4869 <radwaahmed4869@gmail.com>
2023-01-19 06:57:07 +0200

    Add push sound

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Image tut;
+    private void Start()
+    {
+        tut.gameObject.SetActive(false);
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(tutorial());
     }
 
     public void QuitGame()
     {
         Application.Quit();  
     }
-    
+
+    IEnumerator tutorial()
+    {
+        tut.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        tut.gameObject.SetActive(false);
+    }
+
 }
